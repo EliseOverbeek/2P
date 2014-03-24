a6f86222507235195e2901d05a4580a6000a5291
Peter Beers <pwbeers@gmail.com>
2014-03-24 18:02:06 +0100

    Aanmaken van Board
    Board klasse aangemaakt en methodes toegevoegd. Deels geimplementeerd en
    van javadoc voorzien

--- model/Board.java
+++ model/Board.java
@@ -2,16 +2,55 @@ package model;
 import java.util.ArrayList;
 
 /**
- * 
- *  * @author EliseOverbeek
- *  18-3-2014
- *  Klasse die het spelbord modelleerd
+ * Board. Klasse die een bord bijhoudt voor het spel met Field objecten
+ * Eindproject PentagoXL Programmeren 2
  *
+ * @author Elise Overbeek en Peter Beers
+ * @version $Revision: 1.0 $
  */
-
-
 
 public class Board {
 	
-
+	private ArrayList<Field> fields;
+	
+	/**
+	 * 	Maakt een bord aan met velden
+	 */
+	public Board()	{
+		fields = new ArrayList<Field>();
+		while (fields.size() <81)	{
+			fields.add(new Field());
+		}
+	}
+	
+	/**
+	 * Vul een bepaald veld met de meegegeven waarde color
+	 * @param field <= 81 && field >=0
+	 * @param color <= 4 && field >=0
+	 */
+	public void fillField(int field, int color)	{
+		fields.get(field).fillField(color);
+	}
+	
+	public boolean checkField(int field)	{
+		boolean check = false;
+		if()
+		return check;
+	}
+	
+	public void rotate(int fieldArea, char rotation)	{
+		
+	}
+	
+	public boolean diagonalWinner()	{
+		return false;
+	}
+	
+	public boolean horizontalWinner()	{
+		return false;
+	}
+	
+	public boolean verticalWinner()	{
+		return false;
+	}
 }
