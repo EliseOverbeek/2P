46ebecd9592977f858c5eee5aa78096bb61a819e
EliseOverbeek <elise.overbeek@live.nl>
2014-03-25 18:00:20 +0100

    adding javadoc

--- model/Stones.java
+++ model/Stones.java
@@ -12,33 +12,42 @@ public class Stones {
 	private int stoneAmount;
 
 	/**
-	 * klasse waarin de knikkers van een speler worden bijgehouden.
+	 * constructor for the class Stones initialize the value of stones at 0
 	 * 
 	 */
-
 	public void Stones() {
 		// bij het aanmaken van de stenen is nog niet altijd bekend hoeveel
 		// stenen er zijn. Daarom begin je met 0 stenen.
 		stoneAmount = 0;
 	}
 
+	/**
+	 * Method to set the amount of stones a player has
+	 * 
+	 * @param StoneAmount
+	 */
 	public void setStoneAmount(int StoneAmount) {
-	
+
 		stoneAmount = StoneAmount;
 	}
 
+	/**
+	 * 
+	 * @return wether or not the removal of a stone from the total stone amount
+	 *         was succesful.
+	 */
 	public boolean removeStone() {
-		boolean gelukt = false; // het verwijderen van de steen is (nog) niet
-								// gelukt.
+		boolean removal = false; // het verwijderen van de steen is (nog) niet
+									// gelukt.
 
 		if (stoneAmount > 0) {
 			// verwijderen van de steen lukt, tel er 1 af en geef terug dat het
 			// gelukt is.
 			stoneAmount--;
-			gelukt = true;
+			removal = true;
 		}
 
-		return gelukt;
+		return removal;
 	}
 
 }
