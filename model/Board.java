8f5c1f6d6c5af1deb443dda94144abd72bfaf643
Peter Beers <pwbeers@gmail.com>
2014-03-26 11:48:12 +0100

    Board aangepast
    Nieuwe arraylist van arraylist<Field> verwerkt

--- model/Board.java
+++ model/Board.java
@@ -11,37 +11,40 @@ import java.util.ArrayList;
 
 public class Board {
 	
-	private ArrayList<Field> fields;
+	private final int DIM = 9;
+	private ArrayList<ArrayList<Field>> fields;
 	
 	/**
 	 * 	Maakt een bord aan met velden
 	 */
 	public Board()	{
-		fields = new ArrayList<Field>();
-		while (fields.size() <81)	{
-			fields.add(new Field());
+		fields = new ArrayList<ArrayList<Field>>();
+		for (int y=0; y<DIM; y++)	{
+			fields.add(y,new ArrayList<Field>());
+			for (int x=0; x<DIM; x++)	{
+				fields.get(y).add(x, new Field());
+			}
 		}
 	}
 	
 	/**
 	 * Vul een bepaald veld met de meegegeven waarde color
-	 * @param field <= 81 && field >=0
-	 * @param color <= 4 && field >=0
+	 * @param y < DIM && y >=0
+	 * @param x < DIM && x >=0
+	 * @param color < 4 && field >=0
 	 */
-	public void fillField(int field, int color)	{
-		fields.get(field).fillField(color);
+	public void fillField(int x, int y, int color)	{
+		fields.get(y).get(x).fillField(color);
 	}
+	
 	/**
 	 * Kijkt of een bepaald veld leeg is of niet 
-	 * @param field is een integer voor een bepaald veld
+	 * @param y < DIM && y >=0
+	 * @param x < DIM && x >=0
 	 * @return TRUE als het veld leeg is, False als het veld bezet is
 	 */
-	public boolean checkField(int field)	{
-		boolean check = false;
-		if(fields.get(field).isEmpty())	{
-			check = true;
-		}
-		return check;
+	public boolean checkField(int x, int y)	{
+		return fields.get(y).get(x).isEmpty();
 	}
 	
 	/**
@@ -49,20 +52,20 @@ public class Board {
 	 * @param fieldArea is een int van 0 t/m 8 die een bepaald gedeelte van het bord representeerd
 	 * @param rotation is een char die door middel van 'l' aangeeft dat fieldArea links moet omkeren en door middel van 'r' aangeeft dat fieldArea recht moet omkeren
 	 */
-	public void rotate(int fieldArea, char rotation)	{
+/*	public void rotate(int fieldArea, char rotation)	{
 		int[] area = fieldsArea(fieldArea);
 		int areaPoint = 0;
 		while (areaPoint < 9)	{
 			// slimme implementatie nog voor verzinnen
 		}
 	}
-	
+*/	
 	/**
 	 * Maakt een lijst aan met daarin de velden van een bepaal gebied
 	 * @param fieldArea < 9 && fieldArea >=0
 	 * @return een array
 	 */
-	private int[] fieldsArea(int fieldArea)	{
+/*	private int[] fieldsArea(int fieldArea)	{
 		int[] area = new int[9];
 		int pointer = 0;
 		int lastfield;
@@ -92,7 +95,7 @@ public class Board {
 		}
 		return area;
 	}
-	
+*/	
 	/**
 	 * Controleerd of er 5 elementen op een rij van dezelfde kleur en diagonaal zijn
 	 * @return TRUE als er 5 elementen van dezelfde kleur diagonaal zijn, FALSE als er geen 5 elementen van dezelfde kleur diagonaal zijn
@@ -105,7 +108,7 @@ public class Board {
 	 * Controleerd of er 5 elementen op een rij van dezelfde kleur en horizontaal zijn
 	 * @return TRUE als er 5 elementen van dezelfde kleur horizontaal zijn, FALSE als er geen 5 elementen van dezelfde kleur horizontaal zijn
 	 */
-	public boolean horizontalWinner()	{
+/*	public boolean horizontalWinner()	{
 		boolean winner = false;
 		int line = 0;
 		ArrayList<Field> value = fields;
@@ -127,7 +130,7 @@ public class Board {
 		}
 		return winner;
 	}
-
+*/
 	/**
 	 * Controleerd of er 5 elementen op een rij van dezelfde kleur en verticaal zijn
 	 * @return TRUE als er 5 elementen van dezelfde kleur verticaal zijn, FALSE als er geen 5 elementen van dezelfde kleur verticaal zijn
